6e21abd1ec10c5b4ebe869fed4e84c523ea782bd
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2025-12-26 00:33:52 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    double num = 0;
+    double sum = 0;
+    string line;
+    ifstream pad("score.txt");
+    double sumSquares;
+    while(getline(pad,line)){
+        num = stod(line);
+        sum += num;
+        sumSquares += pow(num,2);
+        count ++;
+    }
+    
+    double mean = sum / count;
+    double stdev = sqrt(sumSquares / count - pow(mean,2) );
+    
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum / count << endl;
+    cout << "Standard deviation = " << stdev << endl;
+    pad.close();
+}
